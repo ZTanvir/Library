9ace4a483eddfa81eba3e261ad79ec9c7475b7cd
zahirul <zahirulopel@gmail.com>
2022-09-24 15:30:53 +0600

    Display information of Book as table

--- script/script.js
+++ script/script.js
@@ -20,3 +20,44 @@ function addBookToLibrary(bookTitle, bookAuthor, bookPages, status) {
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 208, true);
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
 addBookToLibrary("Moby Dick", "Herman Melville", 635, true);
+
+// Select table element
+const tableEl = document.querySelector(".book-information");
+console.log(tableEl);
+
+// create table head and body
+const tblHead = document.createElement("thead");
+const tblBody = document.createElement("tbody");
+// insert item in table head
+for (let item of myLibrary) {
+    let headRow = document.createElement("thead");
+    console.log("key");
+    for (let key in item) {
+        let colTbl = document.createElement("th");
+        let text = document.createTextNode(
+            `${key[0].toUpperCase()}${key.slice(1)}` //Make the first letter uppercase
+        );
+        colTbl.appendChild(text);
+        headRow.appendChild(colTbl);
+        console.log(key);
+    }
+    // Add table head to table
+    tableEl.appendChild(headRow);
+    break;
+}
+
+// Insert item to table body
+for (let item of myLibrary) {
+    let rowTbl = document.createElement("tr");
+    console.log("Values:");
+    for (let key in item) {
+        let colTbl = document.createElement("td");
+        let text = document.createTextNode(`${item[key]}`);
+        colTbl.appendChild(text);
+        rowTbl.appendChild(colTbl);
+        console.log(item[key]);
+    }
+    tblBody.appendChild(rowTbl);
+    // Add table body to table
+    tableEl.appendChild(tblBody);
+}
